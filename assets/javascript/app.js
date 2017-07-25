cf405cd1d55edbfb5c81f2ed3ac88c8a8ee0f461
Eddie <itein@Hotmail.com>
2017-07-24 20:39:37 -0700

    backup

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {
+
+  var question1 = ["What rarity should all steak be served at?"];
+  var question2 = ["What is ginger used for when eating sushi?"];
+  var question3 = ["Which of these are not authentic mexican food?"];
+
+  var q1answer1 = ["Rare"];
+  var q1answer2 = ["Medium Rare"];
+  var q1answer3 = ["Medium"];
+  var q1answer4 = ["Well Done"];
+
+  var count = 16;
+  var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
+
+  function timer(){
+    count=count-1;
+    if (count <= -1){
+      // alert("Time's Up!  Next question!")
+       return;
+    }
+    $("#timer").html(count + " secs");
+  }
+
+  $("#trivia").html(question1);
+      console.log(question1);
+
+    if(question1){
+      $("#choice1").html(q1answer1);
+
+      $("#choice2").html(q1answer2);
+
+      $("#choice3").html(q1answer3);
+
+      $("#choice4").html(q1answer4);
+
+    }
+
+
+
+
+
+
+
+
+});
