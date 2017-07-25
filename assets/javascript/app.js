ffd891b66dccd70d1f1d51010fe25dee313229b5
Core <itein@hotmail.com>
2017-07-24 23:28:34 -0700

    added start

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   var counter = setInterval(timer, 1000); //1000 will  run it every 1 second
 
   function timer(){
-    count=count-1;
+    count = count - 1;
     if (count <= -1){
       // alert("Time's Up!  Next question!")
        return;
@@ -23,18 +23,19 @@ $(document).ready(function() {
 
   $("#trivia").html(question1);
       console.log(question1);
-
     if(question1){
       $("#choice1").html(q1answer1);
-
       $("#choice2").html(q1answer2);
-
       $("#choice3").html(q1answer3);
-
       $("#choice4").html(q1answer4);
-
     }
 
+    $('#choice1').on("click", function() {
+  	      amp = amp + cryst1;
+            console.log (amp + " c1 amped up");
+          $("#cAmp").html(amp);
+          clickTotal();     //run function to check if need to update win/loss
+        })
 
 
 
