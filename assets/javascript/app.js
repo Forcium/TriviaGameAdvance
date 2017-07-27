314707e890c39d179163b1a52650ee4b8c06580f
Eddie <itein@Hotmail.com>
2017-07-26 18:22:58 -0700

    breakthrough

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
-  var question1 = ["What rarity should all steak be served at?"];
-  var question2 = ["What is ginger used for when eating sushi?"];
-  var question3 = ["Which of these are not authentic mexican food?"];
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~Global Variables~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  var question = ["What rarity should all steak be served at?", "What is ginger used for when eating sushi?", "Which of these are not authentic mexican food?"];
 
   var correct = 0;
   var incorrect = 0;
@@ -10,45 +9,84 @@ $(document).ready(function() {
   var q1answer = ["Rare", "Medium Rare", "Medium", "Well Done"];
   var q2answer = ["Spicing it up", "Garnish", "Cleaning taste palate", "Cover up the wasabi taste"];
   var q3answer = ["Taco", "Tamales", "Enchiladas", "Burritos"];
+  var answerBulk = [q1answer, q2answer, q3answer];
+  var bulk = [];
+  // var quest = [];
 
-
-  var count = 15;
+  var count = 4;
   var counter;
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   function timeDown() {
-      counter = setInterval(decrement, 1000);
-    }
+    counter = setInterval(decrement,1000);
+  }
 
   function decrement() {
-      count--;
-      $("#timerCount").html(count);
+    count--;
+    $(".timerCount").html(count);
+    if ($(".timerCount").html() == 0) {
+      clearInterval(counter);
+      showQuestions();
+      showAnswers();
 
-      if (count === 0) {
-        stop();
-        alert("Time Up!");
-      }
     }
-
-  $('#clickMe').on("click", function() {
-
-    $("#clickMe").remove();
-    $("#timerCount").html(count);
-    $("#question").html(question1);
-    $("#answer1").html(q1answer[0]);
-    $("#answer2").html(q1answer[1]);
-    $("#answer3").html(q1answer[2]);
-    $("#answer4").html(q1answer[3]);
-
-    timeDown();
-  })
-
-    // count = count - 1;
-    // if (count <= -1){
-    //   $("#innerFront").html('<img src="../assets/images/time_mediumrare.jpg" />'); //change to image that shows "TIME OUT!"
-    //    return;
-    // }
-    //
-    //   $("#timerCount").html(count + " secs");
+  }
 
 
-});
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~Start - Click~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    $('#clickMe').on("click", function() {
+      $("#clickMe").remove();
+      $(".question").html(question[0]);
+      $(".answer1").html(answerBulk[0][0]);
+      $(".answer2").html(answerBulk[0][1]);
+      $(".answer3").html(answerBulk[0][2]);
+      $(".answer4").html(answerBulk[0][3]);
+        timeDown();
+
+
+    }) //click function closing tag
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~For Loop array of questions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function showQuestions() {
+        for (i = 0; i < question.length; i++){
+          if ($(".question").html() === question[i]){
+
+
+                $(".question").html(question[i+1]);
+          }
+        }
+      };
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~For Loop array of possible answers~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function showAnswers() {
+        for (x = 0; x < answerBulk.length; x++){
+          if ($(".answer1").html() === answerBulk[x][0]){
+
+                bulk.push(answerBulk[x+1]);
+
+                $(".answer1").html(bulk[0][0]);
+          }
+          if ($(".answer2").html() === answerBulk[x][1]){
+
+                bulk.push(answerBulk[x+1]);
+
+                $(".answer2").html(bulk[1][1]);
+
+              // $(".answer2").html(answerBulk[x+1][1]);
+          }
+          if ($(".answer3").html() === answerBulk[x][2]){
+
+                bulk.push(answerBulk[x+1]);
+
+                $(".answer3").html(bulk[2][2]);
+          }
+          if ($(".answer4").html() === answerBulk[x][3]){
+
+                bulk.push(answerBulk[x+1]);
+
+                $(".answer4").html(bulk[3][3]);
+          }
+        }
+      };
+
+
+
+}); //script closing tag
